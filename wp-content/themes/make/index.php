195637c926c6725694573862908446bd7ca8978e
Egg Syntax <egg@novonon.com>
2015-05-07 09:56:15 -0600

    Only "articles" posts on main page

--- wp-content/themes/make/index.php
+++ wp-content/themes/make/index.php
@@ -10,9 +10,13 @@ global $post;
 <?php ttfmake_maybe_show_sidebar( 'left' ); ?>
 
 <main id="site-main" class="site-main" role="main">
-<?php if ( have_posts() ) : ?>
 
-	<?php while ( have_posts() ) : the_post(); ?>
+<!-- Only show posts in category 10 (articles) -->
+<!-- See repo for previous version -->
+<?php $query = new WP_Query( 'cat=10' ); ?>
+<?php if ( $query->have_posts() ) : ?>
+
+	<?php while ( $query->have_posts() ) : $query->the_post(); ?>
 		<?php
 		/**
 		 * Allow for changing the template partial.
